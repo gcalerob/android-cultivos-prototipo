80c76774291179cd6f5e2007d5e3a91d23c8320f
gcalerob <gcalerob@gmail.com>
2016-06-13 16:33:05 +0200

    GPS code to 'NuevoFormulario' activity

--- app/src/main/java/classes/AppApplication.java
+++ app/src/main/java/classes/AppApplication.java
@@ -1,60 +1,9 @@
 package classes;
 
-import android.Manifest;
+
 import android.app.Application;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
-
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-
-
 public class AppApplication extends Application
-        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-
-    public GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
-            .addConnectionCallbacks(this)
-            .addOnConnectionFailedListener(this)
-            .addApi(LocationServices.API)
-            .build();
-
-    Location mLastLocation;
-    public String latitud, longitud;
-
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (mLastLocation != null) {
-            latitud = String.valueOf(mLastLocation.getLatitude());
-            longitud = String.valueOf(mLastLocation.getLongitude());
-        }
-    }
-
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
+        {
 
 }
--- app/src/main/java/com/grupovermon/prototipo_cultivos/NuevoFormulario.java
+++ app/src/main/java/com/grupovermon/prototipo_cultivos/NuevoFormulario.java
@@ -1,39 +1,75 @@
 package com.grupovermon.prototipo_cultivos;
 
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import classes.AppApplication;
 
-public class NuevoFormulario extends AppCompatActivity{
+public class NuevoFormulario extends AppCompatActivity
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    AppApplication mApp;
+    GoogleApiClient mGoogleApiClient;
+
+    Location mLastLocation;
+    String latitud, longitud;
+//    AppApplication mApp;
     TextView tvGPS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rellenar_formulario);
-        mApp = ((AppApplication)getApplicationContext());
+//        mApp = ((AppApplication)getApplicationContext());
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
         tvGPS = (TextView)findViewById(R.id.tvGPS);
-        tvGPS.setText(mApp.latitud + " - " + mApp.longitud);
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        mApp.mGoogleApiClient.connect();
+        mGoogleApiClient.connect();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        if (mApp.mGoogleApiClient.isConnected()) {
-            mApp.mGoogleApiClient.disconnect();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
         }
     }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            latitud = String.valueOf(mLastLocation.getLatitude());
+            longitud = String.valueOf(mLastLocation.getLongitude());
+            tvGPS.setText(latitud + " , " + longitud);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i)  {
+            mGoogleApiClient.connect();
+        }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
